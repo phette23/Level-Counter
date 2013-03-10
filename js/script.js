a4c5cc941ad6ce498239f81d0a25c885c7d5b03a
Eric Phetteplace <phette23@gmail.com>
2013-03-09 21:21:56 -0500

    huge refactor of combat code & HTML

--- js/script.js
+++ js/script.js
@@ -66,125 +66,107 @@ var LevelCounter = (function ($) {
     },
 
     // combat section
-    openCombatDialog = function () {
+    combat = {
+        // all the combat DOM lookups
+        $dialog : $( '#combat-dialog' ),
+        $addPlayerBtn : $( '#add-player' ),
+        $addMonsterBtn : $( '#add-monster' ),
+        $doneBtn : $( '#combat-done' ),
+        // handy strings & info
+        monsterPrompt : 'What is the monster\'s level?',
+        needInt : 'Please enter an integer.',
+        numMonsters : 1
+    };
+
+    // these build on previous properties
+    combat.$firstPlayer = combat.$dialog.find( '.player' );
+    combat.$firstMonster = combat.$dialog.find( '.monster' );
+    // monster HTML, used for adding multiple monsters
+    combat.monsterDOM = combat.$dialog.find( '.monster-col' ).html();
+    combat.playerDOM = combat.$dialog.find( '.player-col' ).html();
+
+    combat.initialize = function () {
+        // set up +/- buttons for monster & player
+        plusMinusBtns( this.$firstMonster );
+        plusMinusBtns( this.$firstPlayer );
+
+        // set up add monster/player buttons
+        this.$addMonsterBtn.click(
+            function () {
+                var newMonsterStrength = prompt( combat.monsterPrompt );
+                while ( isNaN( pI( newMonsterStrength ) ) ) {
+                    newMonsterStrength = prompt( combat.needInt );
+                }
+                // fill in new Monster level & apply new +/- handlers
+                $( '#combat-dialog .monster' ).eq( combat.numMonsters - 1 ).after( combat.monsterDOM );
+                var $newMonster = $( '#combat-dialog .monster' ).eq( combat.numMonsters );
+                $newMonster.find( '.display' ).text( newMonsterStrength );
+                plusMinusBtns( $newMonster );
+                combat.numMonsters++;
+            }
+        );
+
+        this.$addPlayerBtn.click(
+            function () {
+                var helperStrength = prompt( 'What is the player\'s combat strength?' );
+                while ( isNaN( pI( helperStrength ) ) ) {
+                    helperStrength = prompt( combat.needInt );
+                }
+                // fill in helper strength & apply new +/- handlers
+                combat.$firstPlayer.after( combat.playerDOM );
+                var $newPlayer = $( '#combat-dialog .player' ).eq( 1 );
+                $newPlayer.find( '.display' ).text( helperStrength );
+                $newPlayer.find( 'h2' ).text( 'Helper' );
+                plusMinusBtns( $newPlayer );
+                // can't have more than 2 helpers so hide +player
+                combat.$addPlayerBtn.hide();
+            }
+        );
+
+        // shut down if Done is hit
+        this.$doneBtn.click(
+            function () {
+                // hide dialog
+                $( '#combat-dialog' ).hide( 'slow',
+                    // when it's hidden, reset everything to standard
+                    function () {
+                        var numMonsters = $( '.monster' ).length;
+                        // remove helper, show +player again
+                        $( '#combat-dialog .player' ).eq( 1 ).remove();
+                        combat.$addPlayerBtn.show();
+                        // remove extra monsters
+                        for ( var i = 1; i < numMonsters ; i++ ) {
+                            $( '#combat-dialog .monster' ).eq( i - 1 ).remove();
+                        }
+                        numMonsters = 1;
+                    }
+                );
+                // scroll to top, combat dialog has chance to be longer than page
+                window.scrollTo( window.scrollX, 0 );
+            }
+        );
+    };
+
+    combat.openDialog = function () {
         // these vars are shared by the 2 sub-functions below
-        var monsterPrompt = 'What is the monster\'s level?',
-            needInt = 'Please enter an integer.',
-            numMonsters = 1;
+        var monsterStrength = prompt( combat.monsterPrompt );
 
-        // handles 1st time setup: handlers on +monster, +player, done
-            initialSetup = function () {
-                var $combPlayer = $( '#combat-dialog .player' ),
-                    $combMonster = $( '#combat-dialog .monster' ),
-                    $addPlayerBtn = $( '#add-player' );
-                // set up +/- buttons for monster & player
-                plusMinusBtns( $combMonster );
-                plusMinusBtns( $combPlayer );
-
-                // set up add monster/player buttons
-                $( '#add-monster' ).click(
-                    function () {
-                        var newMonsterStrength = prompt( monsterPrompt );
-                        while ( isNaN( pI( newMonsterStrength ) ) ) {
-                            newMonsterStrength = prompt( needInt );
-                        }
-                        // copy & insert 1st monster HTML but not +/- handlers
-                        var monsterDOM = $combMonster.clone( false );
-                        // fill in new Monster level & apply new +/- handlers
-                        $( '#combat-dialog .monster').eq( numMonsters - 1 ).after( monsterDOM );
-                        var $newMonster = $( '#combat-dialog .monster').eq( numMonsters );
-                        numMonsters++;
-                        $newMonster.find( '.display' ).html(
-                            pI( newMonsterStrength )
-                        );
-                        plusMinusBtns( $newMonster );
-                    }
-                );
-
-                $addPlayerBtn.click(
-                    function () {
-                        var helperStrength = prompt( 'What is the player\'s combat strength?' );
-                        while ( isNaN( pI( helperStrength ) ) ) {
-                            helperStrength = prompt( needInt );
-                        }
-                        // copy & insert first player HTML but not +/- handlers
-                        var playerDOM = $combPlayer.first().clone( false );
-                        // fill in helper strength & apply new +/- handlers
-                        $combPlayer.after( playerDOM );
-                        var $newPlayer = $( '.player' ).eq( 1 );
-                        $newPlayer.find( '.display' ).html(
-                            pI( helperStrength )
-                        );
-                        $newPlayer.find( 'h2' ).text( 'Helper' );
-                        plusMinusBtns( $newPlayer );
-                        // can't have more than 2 helpers so hide +player
-                        $addPlayerBtn.hide();
-                    }
-                );
-
-                // shut down if Done is hit
-                $( '#combat-done' ).click(
-                    function () {
-                        // hide dialog
-                        $( '#combat-dialog' ).hide( 'slow',
-                            // when it's hidden, reset everything to standard
-                            function () {
-                                var numMonsters = $( '.monster' ).length;
-                                // remove helper, show +player again
-                                $( '#combat-dialog .player' ).eq( 1 ).remove();
-                                $addPlayerBtn.show();
-                                // remove extra monsters
-                                for ( var i = 1; i < numMonsters ; i++ ) {
-                                    $( '#combat-dialog .monster' ).eq( i - 1 ).remove();
-                                }
-                                numMonsters = 1;
-                            }
-                        );
-                        // scroll to top, combat dialog has chance to be longer than page
-                        window.scrollTo( window.scrollX, 0 );
-                    }
-                );
-
-                // after we're set up, still need to runCombat
-                runCombat();
-            },
-
-            // fills in combat strengths, runs after initialSetup()
-            runCombat = function () {
-                var monsterStrength = prompt( monsterPrompt ),
-                    $combPlayer = $( '#combat-dialog .player' ),
-                    $combMonster = $( '#combat-dialog .monster' );
-
-                while ( isNaN( pI( monsterStrength ) ) ) {
-                    if ( !monsterStrength ) {
-                        // user hit cancel, or entered nothing
-                        return;
-                    }
-                    monsterStrength = prompt( needInt );
-                }
-                $combMonster.find( '.display' ).html( monsterStrength );
-
-                // fill in player's attributes
-                $combPlayer.find( '.display' ).html( player.level + player.bonuses );
-                $combPlayer.find( 'h2' ).text( player.name );
-
-                // at the end of runCombat, reveal the dialog
-                $( '#combat-dialog' ).show( 'slow' );
-            };
-
-        // combat dialog isn't in the DOM, load it via AJAX then do initialSetup()
-        if ( $( '#combat-dialog' ).length === 0 ) {
-            $.get( 'combat.html' , function ( response ) {
-                $( response ).appendTo( '#main' );
-                    initialSetup();
-                }
-            );
+        while ( isNaN( pI( monsterStrength ) ) ) {
+            if ( !monsterStrength ) {
+                // user hit cancel, or entered nothing
+                return;
+            } else {
+                monsterStrength = prompt( combat.needInt );
+            }
         }
+        combat.$firstMonster.find( '.display' ).text( monsterStrength );
 
-        // we grabbed combat.html earlier, runCombat()
-        else {
-            runCombat();
-        }
+        // fill in player's attributes
+        combat.$firstPlayer.find( '.display' ).text( player.level + player.bonuses );
+        combat.$firstPlayer.find( 'h2' ).text( player.name );
+
+        // at the end, reveal the dialog
+        $( '#combat-dialog' ).show( 'slow' );
     },
 
     // open nav menu
@@ -262,12 +244,15 @@ var LevelCounter = (function ($) {
         );
     }
 
-    $( '#dagger' ).click( openCombatDialog );
+    // basic combat event listeners
+    $( '#dagger' ).click( combat.openDialog );
+    combat.initialize();
 
     // expose public items
     var Module = {
         player: player,
-        updatePlayer: updatePlayer
+        updatePlayer: updatePlayer,
+        'combat': combat
     };
     return Module;
 } ( jQuery ));
