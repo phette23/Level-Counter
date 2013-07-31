dc642ebba30b56bec1f525905ed79e310b96a316
Eric Phetteplace <phette23@gmail.com>
2013-07-30 21:22:06 -0400

    make SublimeLinter JSHint happy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,12 @@
+/*global require */
 // Generated on 2013-03-31 using generator-webapp 0.1.5
-'use strict';
 var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
 var mountFolder = function (connect, dir) {
-    return connect.static(require('path').resolve(dir));
+    return connect['static'](require('path').resolve(dir));
 };
 
 module.exports = function (grunt) {
+    'use strict';
     // load all grunt tasks
     require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
