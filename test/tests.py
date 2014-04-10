1b04ef7f0162c72401b41d112c2301d5d7bf333e
phette23 <phette23@gmail.com>
2014-04-10 15:26:56 -0400

    tests: remove unneeded Selenium boilerplate
    speeds up tests even further, from ≈3s to ≈2s

--- test/tests.py
+++ test/tests.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-import unittest, time, re
+import unittest
 
 
 class LevelCounter(unittest.TestCase):
@@ -41,30 +37,6 @@ class LevelCounter(unittest.TestCase):
         # Level 2, Bonus 1, Strength 3
         assert driver.find_element_by_xpath("//div[@id='main']/section[2]/span").text == "1"
         assert driver.find_element_by_xpath("//div[@id='main']/section[3]/span").text == "3"
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException, e: return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
 
     def tearDown(self):
         self.driver.quit()
